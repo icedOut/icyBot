aeb9843fadc3e66ad707f179773f7c6c88985ceb
Philippe Chartrand <dapper2474@gmail.com>
2019-11-09 20:58:40 -0500

    Ajout fonction chien + appel API

--- bot.js
+++ bot.js
@@ -1,39 +1,92 @@
-var Discord = require('discord.io');
-var logger = require('winston');
-var auth = require('./auth.json');
-// Configure logger settings
-logger.remove(logger.transports.Console);
-logger.add(new logger.transports.Console, {
-    colorize: true
+'use strict';
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const { Client, Attachment } = require('discord.js');
+const querystring = require('querystring');
+const r2          = require('r2');
+
+const DOG_API_URL   = "https://api.thedogapi.com/"
+const DOG_API_KEY   = "97bccc14-dd2b-408d-b162-c3db4d60cd33"; // get a free key from - https://thedogapi.com/signup
+const DISCORD_TOKEN = 'NjQyODU4ODE4MTQwODk3MzA1.XcdN6Q.PuY0odzjQCARg2xThPMNuTHUv2s';
+
+client.on('ready', () => {
+  console.log(`Icy2Ready`);
 });
-logger.level = 'debug';
-// Initialize Discord Bot
-var bot = new Discord.Client({
-   token: auth.token,
-   autorun: true
+
+client.on('message', msg => {
+  switch (msg.content){
+	  
+	  case '!ping':
+		   msg.reply('Pong!');
+		   break;
+	   
+	  case '!cat':
+		 // Create the attachment using Attachment
+        const cat1 = new Attachment('https://i.imgur.com/SGfRJG7.jpg');
+		const cat2 = new Attachment('https://i.imgur.com/SSN26nN.jpg');
+		var myArray = [cat1 , cat2]
+		var rand = myArray[Math.floor(Math.random() * myArray.length)];
+        // Send the attachment in the message channel
+        msg.channel.send(rand);
+		break;
+		
+	  case '!dog':
+	    messageRecieved(msg);
+	    break;
+		
+  }
+  
 });
-bot.on('ready', function (evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
-});
-bot.on('message', function (user, userID, channelID, message, evt) {
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
-       
-        args = args.splice(1);
-        switch(cmd) {
-            // !ping
-            case 'ping':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Pong!'
-                });
-            break;
-            // Just add any case commands if you want to..
-         }
-     }
-});
+
+client.login(DISCORD_TOKEN);
+
+async function messageRecieved(message)
+{
+  try{
+    // pass the name of the user who sent the message for stats later, expect an array of images to be returned.
+    var images = await loadImage(message.author.username);
+
+    // get the Image, and first Breed from the returned object.
+    var image = images[0];
+    var breed = image.breeds[0];
+
+    console.log('message processed','showing',breed)
+    // use the *** to make text bold, and * to make italic
+    message.channel.send( "***"+breed.name + "*** \r *"+breed.temperament+"*", { files: [ image.url ] } );
+    // if you didn't want to see the text, just send the file
+
+  }catch(error)
+  {
+    console.log(error)
+  }
+}
+/**
+ * Makes a request to theDogAPI.com for a random dog image with breed info attached.
+ */
+async function loadImage(sub_id)
+{
+  // you need an API key to get access to all the iamges, or see the requests you've made in the stats for your account
+  var headers = {
+      'X-API-KEY': DOG_API_KEY,
+  }
+  var query_params = {
+    'has_breeds':true, // we only want images with at least one breed data object - name, temperament etc
+    'mime_types':'jpg,png', // we only want static images as Discord doesn't like gifs
+    'size':'small',   // get the small images as the size is prefect for Discord's 390x256 limit
+    'sub_id': sub_id, // pass the message senders username so you can see how many images each user has asked for in the stats
+    'limit' : 1       // only need one
+  }
+  // convert this obejc to query string 
+  let queryString = querystring.stringify(query_params);
+
+  try {
+    // construct the API Get request url
+    let _url = DOG_API_URL + `v1/images/search?${queryString}`;
+    // make the request passing the url, and headers object which contains the API_KEY
+    var response = await r2.get(_url , {headers} ).json
+  } catch (e) {
+      console.log(e)
+  }
+  return response;
+
+}
