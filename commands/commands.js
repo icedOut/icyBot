57801562187df94ddbefd37ccd78b6c2a69b32a4
Philippe Chartrand <dapper2474@gmail.com>
2019-11-12 17:40:13 -0500

    fixed typo in commands

--- commands/commands.js
+++ commands/commands.js
@@ -6,7 +6,7 @@ ex.addField('!cat' , ' Pictures of cats ' )
 ex.addField('!dog' , ' Pictures of dogs ' )
 ex.addField('!play  - Your search - '  , ' Searches youtube with the argument provided, and plays found song in users voice channel')
 ex.addField('!stop' , ' Stops the musicbot')
-ex.addField('!pause' , ' Pauses the music')
+ex.addField('!skip' , ' skips to the  the song in queue')
 ex.addField('!ping' , 'pong')
 ex.addField('!opgg -LOL username -  (no space)' , 'Searches OP.gg with username provided in argument. Do not add spaces in the ID search' )
 module.exports = {
