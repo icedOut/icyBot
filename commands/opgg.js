5bd7b7fd2a6056f5e47a821a133bc188bd438907
Philippe Chartrand <dapper2474@gmail.com>
2019-11-12 23:23:52 -0500

    opgg command rework in progress

--- commands/opgg.js
+++ commands/opgg.js
@@ -2,9 +2,7 @@ module.exports = {
 	name: 'opgg',
 	description: 'OP.gg summoner level lookup!',
 	execute(message, args) {
-var opn =require('opn');
- var url = 'https://na.op.gg/summoner/userName='+args[0]
- opn(url)
+        message.channel.send('FUNCTION IN CONSTRUCTION');
 }
 
 }
