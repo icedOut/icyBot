c38953e3044d9e35d281007bd305d68fa1fedc7f
Philippe Chartrand <dapper2474@gmail.com>
2019-11-14 00:59:08 -0500

    edit emoji test

--- commands/emoji.js
+++ commands/emoji.js
@@ -3,7 +3,6 @@ module.exports = {
 	name: 'emoji',
 	description: 'generate random emoji',
 	execute(message, args) {
-        console.log("fuck u");
         x =getEmoji();
         console.log(x);
         message.channel.send(x);
